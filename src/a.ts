8ce9522b27d7fc715ff7c13994d3d995f53d3c1d
Rasuljonov <Rasuljonov@Muhammad>
2023-11-17 11:31:23 +0500

    salom

--- src/a.ts
+++ src/a.ts
@@ -1,5 +1,5 @@
 class Car {
- constructor(public name: string) {}
+	constructor(public name: string) {}
 }
 
 class ElectroCar extends Car {}
@@ -7,88 +7,90 @@ class PetrolCar extends Car {}
 class HybridCar extends Car {}
 
 interface Capacity {
- electroCar: number;
- petrolCar: number;
- HybridCar: number;
+	electroCar: number;
+	petrolCar: number;
+	HybridCar: number;
 }
 
 interface Pricing {
- electroCarPricePerMinute: number;
- petrolCarPricePerMinute: number;
- HybridCarPricePerMinute: number;
+	electroCarPricePerMinute: number;
+	petrolCarPricePerMinute: number;
+	HybridCarPricePerMinute: number;
 }
 
 class Parking<T extends Car> {
- public cars: T[] = [];
- public capacity: Capacity = {} as Capacity;
- public pricing: Pricing = {} as Pricing;
+	public cars: T[] = [];
+	public capacity: Capacity = {} as Capacity;
+	public pricing: Pricing = {} as Pricing;
 
- constructor(public name: string, capacity: Capacity, pricing: Pricing) {
-     this.capacity = capacity;
-     this.pricing = pricing;
- }
+	constructor(public name: string, capacity: Capacity, pricing: Pricing) {
+		console.log((this.capacity = capacity));
+		console.log((this.pricing = pricing));
 
- enterCar(car: T): { minutes: number } {
-     const carType = this.getCarType(car);
-     if (this.canParkCar(carType)) {
-         this.cars.push(car);
-         return { minutes: 5 }; // or any other default duration
-     } else {
-         console.log(`Cannot park ${carType} - Parking is full for this type.`);
-         return { minutes: 0 };
-     }
- }
+		this.capacity = capacity;
+		this.pricing = pricing;
+	}
 
- logoutCar(): { totalPrice: number } {
-     // Assuming there is a method to get the car type and calculate the total price
-     const carType = this.getCarType(this.cars[0]); // Assuming there's at least one car
-     const totalPrice = this.calculateTotalPricePerCar(carType);
-     this.cars.pop(); // Remove the logged-out car from the array
-     return { totalPrice };
- }
+	enterCar(car: T): { minutes: number } {
+		const carType = this.getCarType(car);
+		if (this.canParkCar(carType)) {
+			this.cars.push(car);
+			return { minutes: 10 }; // necha minut turishi
+		} else {
+			console.log(` ${carType} - Ushbu turdagi mashinalar to'la.`);
+			return { minutes: 0 };
+		}
+	}
 
- calculateTotalPricePerCar(carType: string): number {
-     const pricePerMinute = this.pricing[`${carType}PricePerMinute`];
-     const totalMinutes = 5; // or any other calculation based on your system
-     return pricePerMinute * totalMinutes;
- }
+	 logoutCar(): { totalPrice: number } {
 
- calculateTotalProfit(): number {
-     let totalProfit = 0;
-     this.cars.forEach((car) => {
-         const carType = this.getCarType(car);
-         totalProfit += this.calculateTotalPricePerCar(carType);
-     });
-     return totalProfit;
- }
+	     const carType = this.getCarType(this.cars[0]);
+	     const totalPrice = this.calculateTotalPricePerCar(carType);
+	     this.cars.pop();
+	     return { totalPrice };
+	 }
 
- private getCarType(car: T): string {
-     if (car instanceof ElectroCar) {
-         return 'electroCar';
-     } else if (car instanceof PetrolCar) {
-         return 'petrolCar';
-     } else if (car instanceof HybridCar) {
-         return 'HybridCar';
-     }
-     throw new Error('Invalid car type');
- }
+	 calculateTotalPricePerCar(carType: string) {
+	  const pricePerMinute = this.pricing `${carType}PricePerMinute`;
+	  const totalMinutes = 5;
+	  return pricePerMinute * totalMinutes;
+	}
 
- private canParkCar(carType: string): boolean {
-     return this.cars.length < this.capacity[carType];
- }
+	calculateTotalProfit() {
+		let totalProfit = 0;
+		this.cars.forEach((car) => {
+			const carType = this.getCarType(car);
+			totalProfit += this.calculateTotalPricePerCar(carType);
+		});
+		return totalProfit;
+	}
+
+	private getCarType(car: T) {
+		if (car instanceof ElectroCar) {
+			return "electroCar";
+		} else if (car instanceof PetrolCar) {
+			return "petrolCar";
+		} else if (car instanceof HybridCar) {
+			return "HybridCar";
+		}
+		throw new Error("Avtomobil turi yoq");
+	}
+
+	private canParkCar(carType: string) {
+		return this.cars.length < this.capacity.HybridCar;
+	}
 }
 
-// Example usage:
 const capacity: Capacity = {
- electroCar: 4,
- petrolCar: 12,
- HybridCar: 3,
+	electroCar: 4,
+	petrolCar: 12,
+	HybridCar: 3,
 };
 
 const pricing: Pricing = {
- electroCarPricePerMinute: 10,
- petrolCarPricePerMinute: 4,
- HybridCarPricePerMinute: 20,
+	electroCarPricePerMinute: 10,
+	petrolCarPricePerMinute: 4,
+	HybridCarPricePerMinute: 20,
 };
 
 const parking = new Parking<Car>("Sebzor", capacity, pricing);
@@ -96,8 +98,8 @@ const parking = new Parking<Car>("Sebzor", capacity, pricing);
 const car1 = new ElectroCar("BYD Sons");
 const car2 = new PetrolCar("Petrol Car 1");
 
-console.log(parking.enterCar(car1)); // { minutes: 5 }
-console.log(parking.enterCar(car2)); // { minutes: 5 }
-console.log(parking.calculateTotalProfit()); // Total profit from parked cars
-console.log(parking.logoutCar()); // { totalPrice: 50 }
-console.log(parking.calculateTotalProfit()); // Updated total profit after logging out a car
+console.log(parking.enterCar(car1));
+console.log(parking.enterCar(car2));
+console.log(parking.calculateTotalProfit());
+console.log(parking.logoutCar());
+console.log(parking.calculateTotalProfit());
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-class Car {}
+import "./a";
+class Car {
+	constructor(public name: string) {}
+}
 
 class ElectroCar extends Car {}
-
 class PetrolCar extends Car {}
-
 class HybridCar extends Car {}
 
 interface Capacity {
@@ -19,28 +20,66 @@ interface Pricing {
 }
 
 class Parking<T extends Car> {
-	public name: string;
 	public cars: T[] = [];
-	public capacity: Capacity;
-	public pricing: Pricing;
+	public capacity: Capacity = {} as Capacity;
+	public pricing: Pricing = {} as Pricing;
 
-	constructor(name: string, capacity: Capacity, pricing: Pricing) {
-		console.log(this.name = name);
+	constructor(public name: string, capacity: Capacity, pricing: Pricing) {
+		console.log((this.capacity = capacity));
+		console.log((this.pricing = pricing));
 
-		console.log(this.capacity = capacity);
-
-
-console.log(		this.pricing = pricing);
-
+		this.capacity = capacity;
+		this.pricing = pricing;
 	}
 
-	enterCar(car: T) {}
+	enterCar(car: T): { minutes: number } {
+		const carType = this.getCarType(car);
+		if (this.canParkCar(carType)) {
+			this.cars.push(car);
+			return { minutes: 10 }; // necha minut turishi
+		} else {
+			console.log(` ${carType} - Ushbu turdagi mashinalar to'la.`);
+			return { minutes: 0 };
+		}
+	}
 
-	logoutCar(car: T) {}
+	 logoutCar(): { totalPrice: number } {
 
-	calculateTotalPricePerCar(carID: string) {}
+	     const carType = this.getCarType(this.cars[0]);
+	     const totalPrice = this.calculateTotalPricePerCar(carType);
+	     this.cars.pop();
+	     return { totalPrice };
+	 }
 
-	calculateTotalProfit() {}
+	 calculateTotalPricePerCar(carType: string) {
+	  const pricePerMinute = this.pricing `${carType}PricePerMinute`;
+	  const totalMinutes = 5;
+	  return pricePerMinute * totalMinutes;
+	}
+
+	calculateTotalProfit() {
+		let totalProfit = 0;
+		this.cars.forEach((car) => {
+			const carType = this.getCarType(car);
+			totalProfit += this.calculateTotalPricePerCar(carType);
+		});
+		return totalProfit;
+	}
+
+	private getCarType(car: T) {
+		if (car instanceof ElectroCar) {
+			return "electroCar";
+		} else if (car instanceof PetrolCar) {
+			return "petrolCar";
+		} else if (car instanceof HybridCar) {
+			return "HybridCar";
+		}
+		throw new Error("Avtomobil turi yoq");
+	}
+
+	private canParkCar(carType: string) {
+		return this.cars.length < this.capacity.HybridCar;
+	}
 }
 
 const capacity: Capacity = {
@@ -55,52 +94,51 @@ const pricing: Pricing = {
 	HybridCarPricePerMinute: 20,
 };
 
-const parking = new Parking<ElectroCar>("Sebzor", capacity, pricing);
-const car1 = new ElectroCar();
-parking.enterCar(car1);
-parking.logoutCar(car1);
+const parking = new Parking<Car>("Sebzor", capacity, pricing);
+
+const car1 = new ElectroCar("BYD Sons");
+const car2 = new PetrolCar("Petrol Car 1");
+
+console.log(parking.enterCar(car1));
+console.log(parking.enterCar(car2));
+console.log(parking.calculateTotalProfit());
+console.log(parking.logoutCar());
+console.log(parking.calculateTotalProfit());
 
 
 
+// // props:   (public)name, (public)sigimi, (public)narxlar
+// // methods:  Methods autokirishi(car: T), autochiqishi(), tolovnaarx(carID: string), jamipul
 
+// /**
+//  *
+//  * Example :
+//  *
+//  * const avtoturargoh = new Avtoturargoh("Sebzor", sigimi, narxlar)
+//  * const car1 = new BYD("BYD Sons")
+//  * avtoturargoh.autokirishi(car1) // { minutes: 5 }
+//  * avtoturargoh.logout(car1) // { totalPrice: 50 }
+//  *
+//  *
+//  * const sigimi: Sigimi = {
+//  *  electroCar: 4,
+//  *  petrolCar: 12,
+//  *  HybridCar: 3,
+//  * }
+//  *
+//  * narxlar = {
+//  *  electroCarPricePerMinute: 10 ,
+//  *  petrolCarPricePerMinute: 4,
+//  *  HybridCarPricePerMinute: 20,
+//  * }
+//  *
+//  */
 
-
-
-
-
-
-// props:   (public)name, (public)sigimi, (public)narxlar
-// methods:  Methods autokirishi(car: T), autochiqishi(), tolovnaarx(carID: string), jamipul
-
-/**
- *
- * Example :
- *
- * const avtoturargoh = new Avtoturargoh("Sebzor", sigimi, narxlar)
- * const car1 = new BYD("BYD Sons")
- * avtoturargoh.autokirishi(car1) // { minutes: 5 }
- * avtoturargoh.logout(car1) // { totalPrice: 50 }
- *
- *
- * const sigimi: Sigimi = {
- *  electroCar: 4,
- *  petrolCar: 12,
- *  HybridCar: 3,
- * }
- *
- * narxlar = {
- *  electroCarPricePerMinute: 10 ,
- *  petrolCarPricePerMinute: 4,
- *  HybridCarPricePerMinute: 20,
- * }
- *
- */
-
-/**
- * Rules:
- *  ✅ Internetdan foydalansa boladi
- *  ✅ Repo yaratish kerak
- *  ✅ UI shart emas
- *  ✅ Loyiha TS da bolishi kerak
- *
- */
+// /**
+//  * Rules:
+//  *  ✅ Internetdan foydalansa boladi
+//  *  ✅ Repo yaratish kerak
+//  *  ✅ UI shart emas
+//  *  ✅ Loyiha TS da bolishi kerak
+//  *
+//  */
